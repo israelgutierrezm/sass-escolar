7c7ff69e2f39e2a6ff9049a845e4e1fc0530dc37
Israel Gutierrez <yosef.gutierrezm@hotmail.com>
2026-07-21 01:28:06 -0600

    chore: fuerza motor InnoDB en la conexión mysql

--- config/database.php
+++ config/database.php
@@ -58,7 +58,9 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
+            // InnoDB explícito: WAMP en esta máquina trae default_storage_engine=MyISAM,
+            // que rompe FKs, transacciones y FOR UPDATE SKIP LOCKED. No es negociable.
+            'engine' => 'InnoDB',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
